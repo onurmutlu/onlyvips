68b59d2482ba4bab61cdd22b8101fb051668ef6f
Onur Mutlu <onurmutlu@gmail.com>
2025-04-12 02:00:59 +0300

    allow origins

--- backend-api/main.py
+++ backend-api/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 # CORS ayarı
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://arayis-evreni.siyahkare.com", "https://arayis-evreni.vercel.app", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
